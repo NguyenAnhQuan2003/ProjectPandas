01e942926ad178b03d48eb44f3040a43a9a44459
nguyenanhquan <quan715105194@gmail.com>
2025-07-13 17:14:35 +0700

    Pandas learning

--- tests/helloPandas.py
+++ tests/helloPandas.py
@@ -5,6 +5,17 @@ mydataset = {
   'passings': [3, 7, 2]
 }
 
+a = [1,2,7]
+
 myvar = pd.DataFrame(mydataset)
+myvar2 = pd.Series(a, index=['Quan', 'Lan', 'Anh'])
+calories = {"day1": 420, "day2": 380, "day3": 390}
+
+myvar3 = pd.Series(calories)
+myvar.loc[0,'cars'] = 'Mec'
 
 print(myvar)
+print(myvar.duplicated())
+print(myvar2)
+print(myvar3)
+print(pd.__version__)
